e61a40b381198ddfd8eec8103c9dd493adc19359
Febriands <38639207+Febriands@users.noreply.github.com>
2019-05-07 09:37:02 +0700

    update apps
    update template of accounts and process

--- csidjango/csidjango/settings.py
+++ csidjango/csidjango/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'process.apps.ProcessConfig',
-    'accounts.apps.ProcessConfig',    
+    'accounts.apps.AccountsConfig',    
 ]
 
 MIDDLEWARE = [
