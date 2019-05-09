962a68fa6255a24fe504c96b432dfb30bf5698db
Febriands <38639207+Febriands@users.noreply.github.com>
2019-05-09 13:37:01 +0700

    Update wsgi.py

--- csidjango/csidjango/wsgi.py
+++ csidjango/csidjango/wsgi.py
@@ -7,10 +7,33 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csidjango.settings')
+
+# application = get_wsgi_application()
+
+
+import os 
+import time 
+import traceback 
+import signal 
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/febriands/public_html/csidjango1/csidjango1/csidjango1') 
+sys.path.append('/home/febriands/env1/lib/python3.6/site-packages')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csidjango.settings')
 
-application = get_wsgi_application()
+try: 
+    application = get_wsgi_application() 
+except Exception: 
+    # Error loading applications 
+    if 'mod_wsgi' in sys.modules: 
+        traceback.print_exc() 
+        os.kill(os.getpid(), signal.SIGINT) 
+        time.sleep(2.5)
