8f8a48b2807b29c3195ae19544ea0349ad914abd
Febriands <38639207+Febriands@users.noreply.github.com>
2019-07-11 15:59:24 +0700

    update app documents
    add date_created at table

--- csidjango/documents/serializers.py
+++ csidjango/documents/serializers.py
@@ -5,15 +5,15 @@ from rest_framework import serializers
 class ManualSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Manual
-        fields = ('id', 'name', 'code', 'version')
+        fields = ('id', 'name', 'code', 'version', 'date_created')
 
 
 class ProcedureSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Procedure
-        fields = ('id', 'name', 'code', 'version')
+        fields = ('id', 'name', 'code', 'version', 'date_created')
 
 class FormSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Form
-        fields = ('id', 'name', 'code', 'version')
+        fields = ('id', 'name', 'code', 'version', 'date_created')
